12df7fb56478869fc4c224749353aa49355af438
jonapoul <jonathanpoulton1@gmail.com>
2017-09-29 11:58:44 +0100

    Added overlaid gaussian, 1/2/3sigma lines from mean, and command line argument for number of runs to simulate

--- checkpoint1/checkpoint1.cpp
+++ checkpoint1/checkpoint1.cpp
@@ -9,22 +9,37 @@
 namespace plt = matplotlibcpp;
 using namespace std;
 
-double rand_range(double fMin, double fMax) {
+double random_in_range(double fMin,
+                       double fMax) {
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
 }
 
-double muon_pdf(double x, double tau) {
+double muon_pdf(double x,
+                double tau) {
    return (1.0/tau) * exp(-x/tau);
 }
 
-int main() {
-   srand(time(NULL));  
+std::pair<std::vector<double>, std::vector<double>> gaussian(double xmin,
+                                                             double xmax,
+                                                             unsigned n_runs,
+                                                             double sigma,
+                                                             double mu) {
+   std::vector<double> x(n_runs), y(n_runs);
+   for (int i = 0; i < n_runs; i++) {
+      x[i] = xmin + i*(xmax-xmin)/(double)n_runs;
+      y[i] = n_runs * 0.045 * exp( -(x[i]-mu)*(x[i]-mu)/(2*sigma*sigma) );
+   }
+   return std::make_pair(x, y);
+}
 
-   double tau = 2.2;//* pow(10, -6);
+int main(int argc, char** argv) {
+   srand(time(NULL));
+
+   double tau = 2.2;//e-6;
 
    int n_tests_per_run = 1000;
-   int n_runs          = 500;
+   int n_runs          = (argc > 1) ? std::stoi(argv[1]) : 500;
 
    double xmin = 0.0;
    double xmax = 10.0*tau;
@@ -34,18 +49,19 @@ int main() {
    vector<double> average_lifetimes(n_runs);
 
    for (int i = 0; i < n_runs; i++) {
-      vector<double> line_x(n_tests_per_run), line_y(n_tests_per_run); // equation line
-      vector<double> points_x(n_tests_per_run), points_y(n_tests_per_run); // generated points under the curve
-      double running_total = 0.0;
+      printf("\rRunning: %.2f%%", i*100.0/(float)n_runs);
+      vector<double> line_x(n_tests_per_run),   line_y(n_tests_per_run);   // pdf equation line
+      vector<double> points_x(n_tests_per_run), points_y(n_tests_per_run); // generated points under the pdf curve
+      double running_total = 0.0; // sum of all decay times that are considered in the simulated mean
 
       for (int j = 0; j < n_tests_per_run; j++) {
          line_x[j] = xmin + (xmax-xmin)*(double)j/(double)n_tests_per_run;
          line_y[j] = muon_pdf(line_x[j], tau);
 
          while (true) {
-            double x = rand_range(xmin, xmax);
+            double x  = random_in_range(xmin, xmax);
             double y1 = muon_pdf(x, tau);
-            double y2 = rand_range(ymin, ymax);
+            double y2 = random_in_range(ymin, ymax);
             if (y2 < y1) {
                points_x[j] = x;
                points_y[j] = y2;
@@ -54,25 +70,43 @@ int main() {
             }
          }
       }
+      // mean lifetime of the muon for this run
       average_lifetimes[i] = running_total / (double)n_tests_per_run;
    }
 
+   // calculating mean and error in the simulated distribution
    double mu = std::accumulate(average_lifetimes.begin(), average_lifetimes.end(), 0.0) / average_lifetimes.size();
-   double sigma = 0;
-   for (auto& a : average_lifetimes) sigma += pow(a-mu, 2);
+   double sigma = 0.0;
+   for (auto& a : average_lifetimes)
+      sigma += pow(a-mu, 2);
    sigma = sqrt(sigma / (average_lifetimes.size()-1));
-   cout << "mu_calculated = " << mu << " sigma = " << sigma << '\n';
+   printf("\rExpected decay time  = %.4f microseconds\n", tau);
+   printf("Simulated decay time = %.4f +- %.4f microseconds\n", mu, sigma);
+   printf("                     = %.2f sigma from expected\n", abs(mu - tau)/sigma);
 
-   plt::hist(average_lifetimes, 50, "b", 0.5);
-   double maximum = 40;
-   plt::named_plot("expected mean",   {tau, tau}, {0, maximum}, "r--");
-   plt::named_plot("calculated mean", {mu, mu},   {0, maximum}, "g--");
-   plt::ylim(0.0, maximum);
+   int number_of_bins = 70;
+   plt::hist(average_lifetimes, number_of_bins, "b", 0.5);
+   double y_maximum = 1.1 * 0.05 * n_runs;
+   plt::named_plot("Expected mean",  {tau, tau}, {0, y_maximum}, "r--");
+   plt::named_plot("Simulated mean", {mu, mu},   {0, y_maximum}, "g--");
 
-   plt::title("Simulated muon decay times");
-   plt::xlabel("Decay time (secs / 1e6)");
+#if 1
+   // lines to represent 1, 2, 3 sigma distance from the mean
+   plt::plot({mu+1*sigma, mu+1*sigma}, {0, y_maximum}, "y-");
+   plt::plot({mu-1*sigma, mu-1*sigma}, {0, y_maximum}, "y-");
+   plt::plot({mu+2*sigma, mu+2*sigma}, {0, y_maximum}, "y-");
+   plt::plot({mu-2*sigma, mu-2*sigma}, {0, y_maximum}, "y-");
+   plt::plot({mu+3*sigma, mu+3*sigma}, {0, y_maximum}, "y-");
+   plt::plot({mu-3*sigma, mu-3*sigma}, {0, y_maximum}, "y-");
+   // overlaid expected gaussian distribution
+   auto overlaid_gaussian = gaussian(mu-sigma*4, mu+sigma*4, n_runs, sigma, mu);
+   plt::named_plot("Expected distribution", overlaid_gaussian.first, overlaid_gaussian.second, "b-");
+#endif
+
+   plt::title(std::to_string(n_runs) + " simulated muon decay times");
+   plt::xlabel("Decay time (microsecs)");
    plt::ylabel("Frequency");
    plt::legend();
-
+   plt::ylim(0.0, y_maximum);
    plt::show();
 }
---
+++ makefile
@@ -0,0 +1,3 @@
+all:
+	(cd checkpoint1 && make)
+	#(cd matrix      && make)
